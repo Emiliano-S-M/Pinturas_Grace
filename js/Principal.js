d3c2408c5671651a816082f0ab61e5205463350e
Emiliano Sanchez M <emilianosm2000.cipn@gmail.com>
2023-07-29 12:22:49 -0600

    Improvement: Deleting unnecessary comments and adding descriptive comments

--- js/Principal.js
+++ js/Principal.js
@@ -1,64 +1,9 @@
-// function mostrarDiv(nombreDiv) {
-//     var contenidos = document.getElementsByClassName("contenido");
-    
-//     for (var i = 0; i < contenidos.length; i++) {
-//         contenidos[i].style.display = "none";
-//     }
-
-//     // Ocultar todos los elementos con la clase "contenido" usando transiciones
-//     setTimeout(function() {
-//         for (var i = 0; i < contenidos.length; i++) {
-//             contenidos[i].classList.add("hidden");
-//         }
-        
-//         var divMostrar = document.getElementById(nombreDiv);
-//         if (divMostrar) {
-//             divMostrar.style.display = "block";
-//             // Mostrar el div con el efecto de desvanecimiento
-//             setTimeout(function() {
-//                 divMostrar.classList.remove("hidden");
-//             }, 20);
-//         }
-
-//     }, 500); // Esperar 0.5 segundos antes de cambiar la visualización
-
-//     var listaLinks = document.querySelectorAll(".menu li a");
-    
-//     listaLinks.forEach(function(link) {
-//         if (link.textContent.toUpperCase() === nombreDiv.toUpperCase()) {
-//             link.classList.add("active-link");
-//             link.classList.remove("inactive-link");
-//         } else {
-//             link.classList.remove("active-link");
-//             link.classList.add("inactive-link");
-//         }
-//     });
-
-//     // Guardar el ID del div en el almacenamiento local
-//     localStorage.setItem("ultimaPosicion", nombreDiv);
-// }
-
-// // Mostrar el div almacenado en el almacenamiento local al cargar la página
-// var ultimaPosicion = localStorage.getItem("ultimaPosicion");
-// if (ultimaPosicion) {
-//     mostrarDiv(ultimaPosicion);
-// } else {
-//     // Mostrar el div "Inicio" por defecto si no hay almacenamiento local
-//     mostrarDiv('inicio');
-// }
-
-// // Evento para guardar la última posición al cerrar la página
-// window.addEventListener("beforeunload", function() {
-//     var divActual = document.querySelector(".contenido[style*='display: block']");
-//     if (divActual) {
-//         var nombreDivActual = divActual.id;
-//         localStorage.setItem("ultimaPosicion", nombreDivActual);
-//     }
-// });
-
+// Función para mostrar un div específico y cambiar la visualización de los enlaces
 function mostrarDiv(nombreDiv) {
+    // Obtener todos los elementos con la clase "contenido"
     var contenidos = document.getElementsByClassName("contenido");
     
+    // Ocultar todos los elementos con la clase "visible" para el efecto de desvanecimiento
     for (var i = 0; i < contenidos.length; i++) {
         contenidos[i].classList.remove("visible");
     }
@@ -69,6 +14,7 @@ function mostrarDiv(nombreDiv) {
             contenidos[i].style.display = "none";
         }
         
+        // Mostrar el div seleccionado
         var divMostrar = document.getElementById(nombreDiv);
         if (divMostrar) {
             divMostrar.style.display = "block";
@@ -80,8 +26,10 @@ function mostrarDiv(nombreDiv) {
 
     }, 500); // Esperar 0.5 segundos antes de cambiar la visualización
 
+    // Obtener todos los elementos de la lista de enlaces
     var listaLinks = document.querySelectorAll(".menu li a");
     
+    // Cambiar las clases de los enlaces para activar/inactivar según el div mostrado
     listaLinks.forEach(function(link) {
         if (link.textContent.toUpperCase() === nombreDiv.toUpperCase()) {
             link.classList.add("active-link");
@@ -92,7 +40,7 @@ function mostrarDiv(nombreDiv) {
         }
     });
 
-    // Guardar el ID del div en el almacenamiento local
+    // Guardar el ID del div en el almacenamiento local para recordar última posición
     localStorage.setItem("ultimaPosicion", nombreDiv);
 }
 
@@ -107,9 +55,12 @@ if (ultimaPosicion) {
 
 // Evento para guardar la última posición al cerrar la página
 window.addEventListener("beforeunload", function() {
+    // Obtener el div actualmente mostrado
     var divActual = document.querySelector(".contenido[style*='display: block']");
     if (divActual) {
+        // Obtener el ID del div actual
         var nombreDivActual = divActual.id;
+        // Guardar el ID del div actual en el almacenamiento local
         localStorage.setItem("ultimaPosicion", nombreDivActual);
     }
 });
--- js/index.js
+++ js/index.js
@@ -1,8 +1,10 @@
+// Elementos del DOM
 const elts = {
 	text1: document.getElementById("text1"),
 	text2: document.getElementById("text2")
 };
 
+// Textos a mostrar
 const texts = [
 	"",
 	"Colores",
@@ -13,14 +15,23 @@ const texts = [
 	"perduran",
 	""
 ];
+
+// Tiempo de la transición del morph
 const morphTime = 1;
+// Tiempo de enfriamiento entre transiciones
 const cooldownTime = 0.25;
+
+// Índice actual del texto
 let textIndex = texts.length - 1;
 let time = new Date();
 let morph = 0;
 let cooldown = cooldownTime;
+
+// Mostrar el primer texto y el siguiente
 elts.text1.textContent = texts[textIndex % texts.length];
 elts.text2.textContent = texts[(textIndex + 1) % texts.length];
+
+// Función para hacer la transición del morph
 function doMorph() {
 	morph -= cooldown;
 	cooldown = 0;
@@ -31,16 +42,23 @@ function doMorph() {
 	}
 	setMorph(fraction);
 }
+
+// Función para aplicar el morph con una fracción dada
 function setMorph(fraction) {
-	
+	// Aplicar efecto de blur y opacidad al texto2
 	elts.text2.style.filter = `blur(${Math.min(8 / fraction - 8, 100)}px)`;
 	elts.text2.style.opacity = `${Math.pow(fraction, 0.4) * 100}%`;
+	// Calcular la fracción invertida para el texto1
 	fraction = 1 - fraction;
+	// Aplicar efecto de blur y opacidad al texto1
 	elts.text1.style.filter = `blur(${Math.min(8 / fraction - 8, 100)}px)`;
 	elts.text1.style.opacity = `${Math.pow(fraction, 0.4) * 100}%`;
+	// Actualizar los textos con los nuevos valores
 	elts.text1.textContent = texts[textIndex % texts.length];
 	elts.text2.textContent = texts[(textIndex + 1) % texts.length];
 }
+
+// Función para hacer el enfriamiento después de la transición
 function doCooldown() {
 	morph = 0;
 	elts.text2.style.filter = "";
@@ -48,6 +66,8 @@ function doCooldown() {
 	elts.text1.style.filter = "";
 	elts.text1.style.opacity = "0%";
 }
+
+// Función de animación principal
 function animate() {
 	requestAnimationFrame(animate);
 	let newTime = new Date();
@@ -64,12 +84,17 @@ function animate() {
 		doCooldown();
 	}
 }
+
+// Llamar a la función de animación
 animate();
 
+// Esperar a que el DOM esté completamente cargado
 document.addEventListener("DOMContentLoaded", function() {
+    // Obtener el elemento con la clase "header" y la duración de la animación CSS
     var animationElement = document.querySelector(".header");
     var animationDuration = getComputedStyle(animationElement).animationDuration;
     animationDuration = parseFloat(animationDuration) * 1000;
+    // Redirigir a la página "Principal.html" después de la duración de la animación
     setTimeout(function() {
         window.location.href = "../html/Principal.html";
     }, animationDuration);
