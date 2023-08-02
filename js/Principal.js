acb39b489610689f7aba17c1e952259062996848
Emiliano Sanchez M <emilianosm2000.cipn@gmail.com>
2023-08-01 23:54:38 -0600

    Implementation of the 'COLORES' section without code optimization

--- js/Principal.js
+++ js/Principal.js
@@ -64,3 +64,125 @@ window.addEventListener("beforeunload", function() {
         localStorage.setItem("ultimaPosicion", nombreDivActual);
     }
 });
+
+
+// // Colores RAL-Classic (algunos ejemplos, puedes agregar más colores según tu necesidad)
+// const ralColors = [
+//     { name: "RAL 1000 BEIGE VERDOSO", hex: "#CDBA88" },
+//     { name: "RAL 2004 NARANJA PURO", hex: "#E97451" },
+//     { name: "RAL 3005 ROJO VINO", hex: "#7B1E3A" },
+//     { name: "RAL 3009 ROJO OXIDO", hex: "#6D342D" },
+//     { name: "RAL 330 30 40 INTENSE MAUVE", hex: "#75476F"}
+//     // Añadir más colores aquí...
+// ];
+
+// // Obtén el elemento del DOM para la lista de colores y el div del nombre del color seleccionado
+// const colorList = document.getElementById("colorList");
+// const selectedColorNameDiv = document.getElementById("selectedColorName");
+
+// // Crea las opciones de colores en la lista
+// ralColors.forEach(color => {
+//     const colorOption = document.createElement("div");
+//     colorOption.classList.add("color-option");
+//     colorOption.style.backgroundColor = color.hex;
+//     colorOption.title = color.name;
+//     colorOption.addEventListener("click", () => {
+//         colorizeImage(color.hex);
+//         showSelectedColorName(color.name);
+//     });
+//     colorList.appendChild(colorOption);
+// });
+
+// // Función para colorear el div con clase "color"
+// function colorizeImage(color) {
+//     const colorDiv = document.querySelector(".canvas .color");
+//     colorDiv.style.backgroundColor = color;
+// }
+
+// // Función para mostrar el nombre del color seleccionado
+// function showSelectedColorName(colorName) {
+//     selectedColorNameDiv.textContent = `Color seleccionado: ${colorName}`;
+// }
+
+// // Llama a la función colorizeImage con el color inicial (por ejemplo, el primer color de la lista)
+// const colorInicial = ralColors[0].hex;
+// colorizeImage(colorInicial);
+// showSelectedColorName(ralColors[0].name); // Mostrar el nombre del color inicial al cargar la página
+
+
+
+// Colores RAL-Classic organizados por categoría tonal
+const ralColors = {
+    Amarillos: [
+        { name: "RAL 1000", hex: "#BEBD7F" },
+        // Añadir más colores amarillos aquí...
+    ],
+    Naranjas: [
+        { name: "RAL 2004", hex: "#E97451" },
+        // Añadir más colores naranjas aquí...
+    ],
+    Rojos: [
+        { name: "RAL 3005", hex: "#7B1E3A" },
+        // Añadir más colores rojos aquí...
+    ],
+    Violetas: [
+        // Añadir más colores violetas aquí...
+    ],
+    Azules: [
+        // Añadir más colores azules aquí...
+    ],
+    Verdes: [
+        // Añadir más colores verdes aquí...
+    ],
+    Grises: [
+        // Añadir más colores grises aquí...
+    ],
+    Marrones: [
+        // Añadir más colores marrones aquí...
+    ],
+    BlancosNegros: [
+        // Añadir más colores blancos y negros aquí...
+    ],
+};
+
+// Obtén el elemento del DOM para la lista de colores, el div del nombre del color seleccionado y la lista desplegable
+const colorList = document.getElementById("colorList");
+const selectedColorNameDiv = document.getElementById("selectedColorName");
+const colorCategorySelect = document.getElementById("colorCategory");
+
+// Función para mostrar los colores de una categoría tonal seleccionada
+function showColorsByCategory() {
+    const selectedCategory = colorCategorySelect.value;
+    colorList.innerHTML = ""; // Limpiamos la lista de colores
+
+    if (ralColors.hasOwnProperty(selectedCategory)) {
+        ralColors[selectedCategory].forEach(color => {
+            const colorOption = document.createElement("div");
+            colorOption.classList.add("color-option");
+            colorOption.style.backgroundColor = color.hex;
+            colorOption.title = color.name;
+            colorOption.addEventListener("click", () => {
+                colorizeImage(color.hex);
+                showSelectedColorName(color.name);
+            });
+            colorList.appendChild(colorOption);
+        });
+    }
+}
+
+// Función para colorear el div con clase "color"
+function colorizeImage(color) {
+    const colorDiv = document.querySelector(".canvas .color");
+    colorDiv.style.backgroundColor = color;
+}
+
+// Función para mostrar el nombre del color seleccionado
+function showSelectedColorName(colorName) {
+    selectedColorNameDiv.textContent = `Color seleccionado: ${colorName}`;
+}
+
+// Llama a la función colorizeImage con el color inicial (por ejemplo, el primer color de la lista)
+const colorInicial = ralColors.Amarillos[0].hex;
+colorizeImage(colorInicial);
+showSelectedColorName(ralColors.Amarillos[0].name); // Mostrar el nombre del color inicial al cargar la página
+showColorsByCategory(); // Mostrar los colores de la categoría Amarillos al cargar la página
